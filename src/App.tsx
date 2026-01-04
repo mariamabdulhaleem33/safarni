d1221a06ec1cba018aa109ecca845302c734267e
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2026-01-04 21:58:00 +0200

    add styles and handle routes in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Search from "./pages/Search/Search";
 import { BrowserRouter } from "react-router-dom";
 import PaymentPage from "./components/checkout/PaymentPage";
+import AppRoutes from "./routes";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Search /> 
-      <PaymentPage />
+    <AppRoutes />
+      {/* <Search />  */}
+      {/* <PaymentPage /> */}
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
