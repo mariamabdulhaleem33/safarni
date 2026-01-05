aae418e5bdfce1656e9df4e85a11c79e12cbb27f
Hussein <130873732+HusseinMohamed41@users.noreply.github.com>
2026-01-05 20:31:28 +0200

    Favorite page deploy

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,51 +10,49 @@ import FlightBooking from "@/pages/flight-booking/FlightBooking";
 import SeatBookingPage from "@/pages/flight-booking/SeatBookingPage";
 import HotelPage from "@/pages/hotel/HotelPage";
 import HotelAboutPage from "@/pages/hotel/HotelAboutPage";
+import { Favorites } from "@/pages/favourite/Favorites";
 import { Provider } from "react-redux";
 import { store } from "@/store";
 
 export default function AppRoutes() {
   return (
     <Provider store={store}>
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route
-          path="favorite"
-          element={
-            <div className="pt-[110px] p-8">
-              <h1>Favorite Page</h1>
-            </div>
-          }
-        />
-        <Route
-          path="compare"
-          element={
-            <div className="pt-[110px] p-8">
-              <h1>Compare Page</h1>
-            </div>
-          }
-        />
-        <Route
-          path="maps"
-          element={
-            <div className="pt-[110px] p-8">
-              <h1>Maps Page</h1>
-            </div>
-          }
-        />
-      </Route>
-      <Route path="/welcome" element={<WelcomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/otp-verify" element={<OTPVerification />} />
-      <Route path="/flight-booking" element={<FlightBooking />} />
-      <Route path="/seat-booking" element={<SeatBookingPage />} />
-      <Route path="/hotel" element={<HotelPage />} />
-      <Route path="hotel/:hotelId" element={<HotelAboutPage />} />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route
+            path="favorite"
+            element={<Favorites />
+            }
+          />
+          <Route
+            path="compare"
+            element={
+              <div className="pt-[110px] p-8">
+                <h1>Compare Page</h1>
+              </div>
+            }
+          />
+          <Route
+            path="maps"
+            element={
+              <div className="pt-[110px] p-8">
+                <h1>Maps Page</h1>
+              </div>
+            }
+          />
+        </Route>
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/otp-verify" element={<OTPVerification />} />
+        <Route path="/flight-booking" element={<FlightBooking />} />
+        <Route path="/seat-booking" element={<SeatBookingPage />} />
+        <Route path="/hotel" element={<HotelPage />} />
+        <Route path="hotel/:hotelId" element={<HotelAboutPage />} />
         <Route path="hotel/:hotelId/:tab" element={<HotelAboutPage />} />
-    </Routes>
+      </Routes>
     </Provider>
   );
 }
