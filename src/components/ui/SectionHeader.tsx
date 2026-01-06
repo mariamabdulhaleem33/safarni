583f83eac34fb650bd73af48d8ef460fb114074e
shaima-metwaly <shimaametwaly331@gmail.com>
2026-01-06 20:17:07 +0200

    adding responsive to recommendation and avilable component

--- src/components/ui/SectionHeader.tsx
+++ src/components/ui/SectionHeader.tsx
@@ -1,15 +1,16 @@
 import type { TSectoinHeaderProps } from "@/types/home.type"
+import React from "react"
 
 
 
 
-const SectionHeader = ({ title, path }: TSectoinHeaderProps) => {
+const SectionHeader = React.memo(({ title, path }: TSectoinHeaderProps) => {
     return (
         <div className="flex justify-between items-center">
-            <h5 className="text-[25px] font-medium text-[#111928] capitalize">{title}</h5>
+            <h5 className="text-[25px] font-medium text-gray-900 capitalize">{title}</h5>
             <button className="text-[#1E429F] text-[22px] font-medium cursor-pointer" onClick={() => console.log(path)}>View all</button>
         </div>
     )
-}
+})
 
 export default SectionHeader
