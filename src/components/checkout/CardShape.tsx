6a9aecc042602fd2080f5fd8fe887c4c3db05846
AhmedElabrass25 <ahmedelabrass2035@gmail.com>
2026-01-07 18:51:15 +0200

    edit the height of images in (payment&success) pages

--- src/components/checkout/CardShape.tsx
+++ src/components/checkout/CardShape.tsx
@@ -1,6 +1,6 @@
 const CardShape = ({ img }: { img: string }) => {
   return (
-    <div className="bg-[#F4F4F4] rounded-[30px] h-120 md:h-183.5 w-full">
+    <div className="bg-[#F4F4F4] rounded-[30px] h-120 md:h-145 w-full">
       <img src={img} className="w-full h-full" alt="card image" />
     </div>
   );
